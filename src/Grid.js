5e015c02ee25f7b4d2ff4b4bf797532d7451e9e5
Ildiko Gaspar <ildiko.gaspar@labs.no>
2020-12-15 11:29:34 +0100

    Renamed column to spanSize

--- src/Grid.js
+++ src/Grid.js
@@ -46,27 +46,27 @@ const Col = styled.div`
     padding: ${rem(24)};
     width: 100%;
 
-    ${({col}) => col === "1" && css `
+    ${({spanSize}) => spanSize === "1" && css `
         width: calc(100%/12);
     `}
 
-    ${({col}) => col === "6" && css`
+    ${({spanSize}) => spanSize === "6" && css`
         width: calc(100%/2);
     ` }
 
-    ${({col}) => col === "4" && css `
+    ${({spanSize}) => spanSize === "4" && css `
         width: calc(100%/3);
     `}
 
-    ${({col}) => col === "3" && css `
+    ${({spanSize}) => spanSize === "3" && css `
         width: calc(100%/4);
     `}
 
-    ${({col}) => col === "5" && css `
+    ${({spanSize}) => spanSize === "5" && css `
         width: calc(100%/5);
     `}
 
-    ${({col}) => col === "2" && css `
+    ${({spanSize}) => spanSize === "2" && css `
         width: calc(100%/6);
     `}
 `
@@ -75,53 +75,53 @@ const Grid = () => {
     return (
             <GridContainer>
                 <Row center >
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col>
-                    <Col col="1">span1</Col> 
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col>
+                    <Col spanSize="1">span1</Col> 
                 </Row>
 
                 <Row spaceBetween alignCenter>
-                    <Col col="6">span6</Col>
-                    <Col col="6" style={{height: "200px"}}>span6</Col>
+                    <Col spanSize="6">span6</Col>
+                    <Col spanSize="6" style={{height: "200px"}}>span6</Col>
                 </Row>
 
                 <Row flexEnd>
-                    <Col col="4">span4</Col>
-                    <Col col="4">span4</Col>
-                    <Col col="4">span4</Col>
+                    <Col spanSize="4">span4</Col>
+                    <Col spanSize="4">span4</Col>
+                    <Col spanSize="4">span4</Col>
                 </Row>
 
                 <Row>
-                    <Col col="3">span3</Col>
-                    <Col col="3">span3</Col>
-                    <Col col="3">span3</Col>
-                    <Col col="3">span3</Col>
+                    <Col spanSize="3">span3</Col>
+                    <Col spanSize="3">span3</Col>
+                    <Col spanSize="3">span3</Col>
+                    <Col spanSize="3">span3</Col>
                 </Row>
 
                 <Row>
-                    <Col col="5">col5</Col>
-                    <Col col="5">col5</Col>
-                    <Col col="5">col5</Col>
-                    <Col col="5">col5</Col>
-                    <Col col="5">col5</Col>
+                    <Col spanSize="5">col5</Col>
+                    <Col spanSize="5">col5</Col>
+                    <Col spanSize="5">col5</Col>
+                    <Col spanSize="5">col5</Col>
+                    <Col spanSize="5">col5</Col>
                 </Row>
 
                 <Row>
-                    <Col col="2">span2</Col>
-                    <Col col="2">span2</Col>
-                    <Col col="2">span2</Col>
-                    <Col col="2">span2</Col>
-                    <Col col="2">span2</Col>
-                    <Col col="2">span2</Col>
+                    <Col spanSize="2">span2</Col>
+                    <Col spanSize="2">span2</Col>
+                    <Col spanSize="2">span2</Col>
+                    <Col spanSize="2">span2</Col>
+                    <Col spanSize="2">span2</Col>
+                    <Col spanSize="2">span2</Col>
                 </Row>
 
             </GridContainer> 
